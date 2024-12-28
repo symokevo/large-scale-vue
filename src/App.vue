bf018b9b9faef7f30c1994959394f4aa31ee4c7c
Simon Rabuogi <symokevo@gmail.com>
2024-12-28 17:28:12 +0300

    Initialize the repository

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import ItemsList component
+import ItemsList from './components/items/ItemsList.vue'
+// mock some data
+const items: any[] = [{
+  id: 1,
+  name: 'Item 1'
+}, {
+  id: 2,
+  name: 'Item 2'
+}, {
+  id: 3,
+  name: 'Item 3'
+}]
+</script>
+
+<template>
+  <div class="home">
+    <ItemsList :items="items"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/items/ItemsList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// items property with a blank array
+defineProps<{ items: any[] }>()
+</script>
+
+<template>
+    <div>
+        <h3>Items:</h3>
+        <ul>
+            <li v-for="(item, index) in items"
+                :key="item.id">
+                {{ item.name }}
+            </li>
+        </ul>
+    </div>
+</template>
